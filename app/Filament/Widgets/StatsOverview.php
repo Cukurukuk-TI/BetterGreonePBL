d74d882fcb2202985b2d548dac172edebd30f1e5
Drenzzz <realdrenzzz@gmail.com>
2025-06-20 16:00:11 +0700

    feat: Menambahkan widget statistik jumlah produk di dasbor admin

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -5,6 +5,7 @@ namespace App\Filament\Widgets;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\User;
+use App\Models\Product;
 
 class StatsOverview extends BaseWidget
 {
@@ -12,10 +13,15 @@ class StatsOverview extends BaseWidget
     {
         return [
             //
-             Stat::make('Total Pelanggan', User::count())
+            Stat::make('Total Pelanggan', User::count())
                 ->description('Jumlah seluruh pelanggan terdaftar')
                 ->descriptionIcon('heroicon-m-users')
                 ->color('success'),
+
+            Stat::make('Total Produk', Product::count())
+                ->description('Jumlah semua varian produk')
+                ->descriptionIcon('heroicon-m-shopping-bag')
+                ->color('warning'),
 
         ];
     }
