a4a9a3c90055ebe8d5709cd80ed53c96112bc997
Drenzzz <realdrenzzz@gmail.com>
2025-06-20 14:21:32 +0700

    refactor: Mengubah rute dasbor utama ke root (/)

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,13 +3,13 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('beranda') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="#" :active="request()->routeIs('beranda')">
+                    <x-nav-link href="{{ route('beranda') }}" :active="request()->routeIs('beranda')">
                         {{ __('Beranda') }}
                     </x-nav-link>
                     <x-nav-link href="#" :active="request()->routeIs('produk')">
--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,8 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('beranda'); // Kita akan ganti nama view-nya di langkah berikutnya
+})->middleware(['auth', 'verified'])->name('beranda'); // Ubah nama rute menjadi 'beranda'
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
